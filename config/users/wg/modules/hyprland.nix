4586e0186a86a75045237d9b2d24b6063301f289
William Greenlee <greenlee04@gmail.com>
2024-07-21 21:37:59 +0000

    fix bug

--- config/users/wg/modules/hyprland.nix
+++ config/users/wg/modules/hyprland.nix
@@ -66,7 +66,7 @@
         animations = {
           enabled = "true";
           bezier = "quart, 0.25, 1, 0.5, 1";
-          animation = [ "windows, 1, 6, quart, slide" "border, 1, 6, quart" "borderangle, 1, 6, quart" "fade, 1, 6, quart" "workspaces, 1, 6, quart" ]
+          animation = [ "windows, 1, 6, quart, slide" "border, 1, 6, quart" "borderangle, 1, 6, quart" "fade, 1, 6, quart" "workspaces, 1, 6, quart" ];
         };
 
         dwindle = {
