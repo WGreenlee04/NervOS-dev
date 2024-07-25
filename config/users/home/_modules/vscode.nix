7122d2e65a57a43f0bdfb4377e61911f3a918f4e
William Greenlee <greenlee04@gmail.com>
2024-07-25 01:10:38 +0000

    add comments

--- config/users/home/_modules/vscode.nix
+++ config/users/home/_modules/vscode.nix
@@ -9,9 +9,9 @@
     home.packages = [ pkgs.nixpkgs-fmt ];
     programs.vscode = {
       enable = true; # temporary text editor
-      extensions = [ pkgs.vscode-extensions.jnoortheen.nix-ide ];
+      extensions = [ pkgs.vscode-extensions.jnoortheen.nix-ide ]; # nix editor extension
       userSettings = {
-        "editor.formatOnSave" = "true";
+        "editor.formatOnSave" = "true"; # use the nixfmt formatter on save
       };
     };
   };
