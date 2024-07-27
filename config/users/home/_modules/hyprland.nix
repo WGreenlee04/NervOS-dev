85a9095e80ffb7a0de8a9658575096dc43665e40
William Greenlee <greenlee04@gmail.com>
2024-07-27 13:43:02 +0000

    switch explorer

--- config/users/home/_modules/hyprland.nix
+++ config/users/home/_modules/hyprland.nix
@@ -42,7 +42,7 @@ in
       settings = {
         monitor = "DP-1,1920x1080@144,0x0,1";
         "$terminal" = "kitty";
-        "$fileManager" = "dolphin";
+        "$fileManager" = "pcmanfm";
         "$taskbar" = "waybar";
         "$menu" = "tofi";
         exec-once = [
