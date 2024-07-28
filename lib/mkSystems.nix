db141758306a68db6f0f8551e4f037f9d385bbe4
William Greenlee <greenlee04@gmail.com>
2024-07-28 19:15:55 +0000

    format and clean

--- lib/mkSystems.nix
+++ lib/mkSystems.nix
@@ -8,15 +8,15 @@ let
   home-manager = inputs.home-manager.nixosModules.home-manager;
   agenix = inputs.agenix.nixosModules.age;
   common = ../config/common.nix;
-  hostConfig = host: import ../config/hosts/${host}.nix;
+  hostConfig = host: ../config/hosts/${host}.nix;
 in
-lib.attrsets.genAttrs hosts (host: 
-  lib.nixosSystem {
-    modules = [
-      home-manager
-      agenix
-      common
-      (hostConfig host)
-    ];
-  }
+lib.attrsets.genAttrs hosts (host:
+lib.nixosSystem {
+  modules = [
+    home-manager
+    agenix
+    common
+    (hostConfig host)
+  ];
+}
 )
