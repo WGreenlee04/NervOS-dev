483b6de966c24be222e3acfdd75590c419430d2c
William Greenlee <greenlee04@gmail.com>
2024-07-29 04:07:57 +0000

    comments

--- config/hosts/_modules/boot.nix
+++ config/hosts/_modules/boot.nix
@@ -17,14 +17,14 @@ in
           useOSProber = true; # detect other boot options (like a Windows installation)
           splashImage = ../_assets/nervos-splash.png; # appears on menu screen
           memtest86.enable = true; # add mem test as a boot option
-          device = "nodev";
+          device = "nodev"; # I think this is only needed for legacy boot?
         };
         efi.canTouchEfiVariables = true; # allows installer to change efi variables (to enable boot options)
       };
       tmp.cleanOnBoot = true; # helps cut down on disk usage, tmp is safe to clean at boot
       plymouth = {
         enable = true; # flicker-free splash screen
-        theme = "spinner";
+        theme = "spinner"; # replaces manufacturer logo
         logo = ../_assets/nervos-splash.png; # logo to be displayed
       };
     };
